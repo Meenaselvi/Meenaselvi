61e3c04aff4ab86202f92c8dbd2916a49a7762dd
Meenaselvi <97466456+Meenaselvi@users.noreply.github.com>
2022-02-06 09:17:53 -0800

    Add files via upload

---
+++ fibonaci.py
@@ -0,0 +1,12 @@
+n=int(input("enter the number:"))
+a=0
+b=1
+sum=0
+while(n<0):
+    print("print greater than 0")
+else:
+    for i in range(0,n):
+        print(sum)
+        a=b
+        b=sum
+        sum=a+b
